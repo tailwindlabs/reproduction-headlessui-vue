ff0902200f14dc9e16c5e3af543d95b57d5d1d6b
Robin Malfait <malfait.robin@gmail.com>
2023-05-09 20:35:00 +0200

    setup Prettier

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
   <div className="grid h-screen w-screen place-content-center">
-    <span>
-      Start editing <code>/src/App.vue</code>
-    </span>
+    <span>Start editing <code>/src/App.vue</code></span>
   </div>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
